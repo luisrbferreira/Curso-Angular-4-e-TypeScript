9384f2185c2797c7f90c54c58566a7c36634c838
Luis Ferreira <luisrbferreira@gmail.com>
2017-10-20 10:45:28 -0300

    Initial commit

---
+++ ES6/arrow_functions.js
@@ -0,0 +1,16 @@
+//ES5
+// var dobroDoValor = function(numero) {
+//     return numero * 2;
+// }
+
+// console.log(dobroDoValor(7));
+
+//ES6 - Arrow Functions
+var dobroDoValor = (numero) => {
+    return numero * 2;
+}
+
+//Mesma função de cima
+var dobroDoValor = numero => numero * 2;
+
+console.log(dobroDoValor(7));
---
+++ ES6/variavel.js
@@ -0,0 +1,8 @@
+var serie = 'Friends'
+
+if(true) {
+    let serie = 'Game of Thrones'
+    console.log(serie);
+}
+
+console.log(serie);
