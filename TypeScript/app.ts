3ea7cb8149570c3c59606aa479faa4fd3876de54
Luis Ferreira <luisrbferreira@gmail.com>
2017-10-20 16:21:47 -0300

    Inferência de Tipos

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,5 +1,22 @@
-let ola = (nome: string) => {
-    console.log("Olá " + nome);
+let ola = (nome: string, sobrenome: string) => {
+    console.log("Olá " + nome + sobrenome);
 }
 
-ola("Luis");
+ola("Luis", "Ferreira");
+
+//Inferência de tipos
+
+let mensagem: string = "Seja bem vindo!!!"
+
+let temporadasFriends: number = 10
+
+let estudandoAngular: boolean = true
+
+let listaDeFrutas: Array<string> = ['Uva', 'Banana', 'Abacaxi']
+//Mesma coisa de cima
+let listaDeFrutas2: string[] = ['Uva', 'Banana', 'Abacaxi']
+
+let notasDasProvas: Array<number> = [7.5, 8, 9]
+
+//Não pode :D
+mensagem = 10
---
+++ TypeScript/dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+var ola = function (nome, sobrenome) {
+    console.log("Olá " + nome + sobrenome);
+};
+ola("Luis", "Ferreira");
+//Inferência de tipos
+var mensagem = "Seja bem vindo!!!";
+var temporadasFriends = 10;
+var estudandoAngular = true;
+var listaDeFrutas = ['Uva', 'Banana', 'Abacaxi'];
+//Mesma coisa de cima
+var listaDeFrutas2 = ['Uva', 'Banana', 'Abacaxi'];
+var notasDasProvas = [7.5, 8, 9];
+//Não pode :D
+mensagem = 10;
