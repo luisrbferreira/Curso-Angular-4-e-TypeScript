1bee6e72dec18ea9d013475ed9e6e54cf19fdd9f
Luis Ferreira <luisrbferreira@gmail.com>
2018-03-27 23:44:35 -0300

    Retomando o curso

--- ES6/variavel.js
+++ ES6/variavel.js
@@ -1,8 +1,46 @@
-var serie = 'Friends'
+var serie = 'Friends' //Escopo global
 
-if(true) {
-    let serie = 'Game of Thrones'
-    console.log(serie);
+if (true) {
+    serie = 'Game of Thrones' //Alterando valor da variavel global
 }
 
 console.log(serie);
+
+//O var neste caso sofre a elevação e a variável sai do escopo do bloco e assume o escopo global
+
+var serie = 'Friends' //Escopo global
+
+if (true) {
+    var serie2 = 'Game of Thrones'
+}
+
+console.log(serie2); //Imprime Game of thrones por conta da elevação
+
+//O let assume o escopo de onde estiver
+
+var serie = 'Friends' //Escopo global
+
+if (true) {
+    let serie2 = 'Game of Thrones'
+}
+
+console.log(serie2); //Vai dar erro pois a variavel existe apenas no escopo do bloco
+
+var serie = 'Friends' //Escopo global
+
+if (true) {
+    let serie2 = 'Game of Thrones'
+    console.log(serie2); //Imprime Game of thrones pois está no contexto do bloco
+}
+
+var serie = 'Friends' //Escopo global
+
+if (true) {
+    let serie = 'Game of Thrones' //Escopo do bloco
+    console.log(serie); //Imprime Game of Thrones
+}
+
+console.log(serie); //Imprime Friends
+
+const serie = 'Friends' //Constante
+serie = 'Game of Thrones' //Dá erro pois o valor não pode ser alterado
