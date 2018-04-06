00c6605deb116d2b59b5717e7a8264cadf1b16b3
Luis Roberto Borba Ferreira <luis.ferreira@SLT-001841.suse>
2018-04-06 18:47:28 -0300

    Verificando frase

--- Projetos/app1/src/app/painel/painel.component.ts
+++ Projetos/app1/src/app/painel/painel.component.ts
@@ -11,10 +11,36 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase:';
+  public resposta: string;
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
 
-  constructor() { console.log(this.frases) }
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase);
+  }
 
   ngOnInit() {
   }
 
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+    // console.log(this.resposta);
+  }
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtbr == this.resposta) {
+
+      alert('A tradução está correta!');
+      
+      //Troca pergunta da rodada
+      this.rodada++;
+
+      // Atualiza o objeto rodadaFrase
+      this.rodadaFrase = this.frases[this.rodada];
+    } else {
+      alert('A tradução está errada!');
+    }
+  }
 }
